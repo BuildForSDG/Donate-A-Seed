745138e2d79c992f7e77a24121c7eac25e54afa0
saintjohnie <nwagbojohn@gmail.com>
2020-05-12 01:40:40 +0100

    i made some fommatting in accordance to the style guideline

--- src/aboutUs.js
+++ src/aboutUs.js
@@ -1,12 +1,12 @@
-//fetching the date
-let display = document.getElementById("time")
-let time = new Date()
-let year = time.getFullYear()
-let month = time.getUTCMonth()
-let day = time.getUTCDate()
-//display date on the UI
-let html = '<h4>%day%.%month%.%year%<br>___<h4>'
-newHtml = html.replace('%day%', day)
-newHtml = newHtml.replace('%month%', month)
-newHtml = newHtml.replace('%year%', year)
-display.insertAdjacentHTML('afterend', newHtml)
+// fetching the date
+const display = document.getElementById('time');
+const time = new Date();
+const year = time.getFullYear();
+const month = time.getUTCMonth();
+const day = time.getUTCDate();
+// display date on the UI
+const html = '<h4>%day%.%month%.%year%<br>___<h4>';
+let newHtml = html.replace('%day%', day);
+newHtml = newHtml.replace('%month%', month);
+newHtml = newHtml.replace('%year%', year);
+display.insertAdjacentHTML('afterend', newHtml);
